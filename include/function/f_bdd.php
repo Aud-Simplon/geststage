744da80f759ec95fb0b53c8b672ec5f0b9982d0c
Aud-Simplon <audrey.luchini@outlook.com>
2024-06-14 09:18:57 +0200

    Update f_bdd.php

--- include/function/f_bdd.php
+++ include/function/f_bdd.php
@@ -4,7 +4,7 @@ function connexionBDD()
 {
 	try
 	{
-            $bdd = new PDO('aud-bdd-geststage.mysql.database.azure.com', 'audrey', 'Simplon2024!', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+            $bdd = new PDO('aud-bdd-geststage.mysql.database.azure.com;port=3306', 'audrey', 'Simplon2024!', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
             return $bdd;
 	}
 	catch(Exception $e)
